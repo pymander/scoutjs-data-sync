ac773359e9b2a75e93d713a24eaab9661a87e53f
Asa Miller <asa@standin.io>
2015-08-14 08:46:17 -0700

    set up crawl

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib');
+// keep in sync with NPM from their couchDB
+var npm = require('./lib/npm.js');
+npm.start();
+
+
+// keep in sync with the data from Github and NPM downloads
+var data = require('./lib/data.js');
+data.start();
--- temp.js
+++ temp.js
@@ -1,8 +1,12 @@
-// var Downloads = require('./lib/downloads.js');
+// var downloads = require('./lib/downloads.js');
+// downloads.update('resty');
 
-// Downloads.update('react-native');
+// var github = require('./lib/github.js');
+// github.update('SoapyIllusions/resty');
 
-var github = require('./lib/github.js');
-// github.update('facebook/react-native');
 
-github.start();
+// var denorm = require('./lib/denorm.js');
+// denorm.update('resty');
+
+var data = require('./lib/data.js');
+data.start();
