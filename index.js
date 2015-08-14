51c43b88507e26ecda49ea7fb7be7390049b43a6
Asa Miller <asa@standin.io>
2015-08-14 09:13:58 -0700

    add hapi to respond to CF healthchecks

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ 
+  host: '0.0.0.0', 
+  port: ~~process.env.PORT || 3000 
+});
+
+server.route({
+  method: 'GET',
+  path:'/', 
+  handler: function (request, reply) {
+    reply('hello world');
+  }
+});
+
+server.start(function() {
+  console.log('Server running at:', server.info.uri);
+});
+
+
+
 // keep in sync with NPM from their couchDB
 var npm = require('./lib/npm.js');
 npm.start();
